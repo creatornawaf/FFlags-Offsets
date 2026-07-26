b8c5e218c312096cca5ec13f881d96529c535a1b
Nawaf Dev <creatornawaf@gmail.com>
2026-07-26 19:27:17 +0300

    Update OtherOffsets.hpp

--- OtherOffsets.hpp
+++ OtherOffsets.hpp
@@ -1,9 +1,23 @@
 #pragma once
+/* =============================================================
+/*                       theo's offsets                         
+/*                  https://offsets.imtheo.lol                  
+/* -------------------------------------------------------------
+/*  Dumped With     : RbxDumperV2                               
+/*  Roblox Version  : version-ed2b47b81f08484a
+/*  Dumper Version  : 2.1.7
+/*  Dumped At       : 14:17 26/07/2026 (GMT)
+/*  Total Offsets   : 393
+/* -------------------------------------------------------------
+/*  Join the discord!                                           
+/*  https://offsets.imtheo.lol/discord                          
+/* =============================================================
+*/
 
 #include <cstdint>
 #include <string>
 namespace Offsets {
-    inline std::string ClientVersion = "version-9affbe66b2624d20";
+    inline std::string ClientVersion = "version-ed2b47b81f08484a";
 
     namespace AirProperties {
          inline constexpr uintptr_t AirDensity = 0x18;
@@ -170,7 +184,7 @@ namespace Offsets {
     }
 
     namespace FakeDataModel {
-         inline constexpr uintptr_t Pointer = 0x85c6908;
+         inline constexpr uintptr_t Pointer = 0x7dccd88;
          inline constexpr uintptr_t RealDataModel = 0x1d0;
     }
 
@@ -348,7 +362,7 @@ namespace Offsets {
          inline constexpr uintptr_t InputObject = 0xf0;
          inline constexpr uintptr_t InputObject2 = 0x100;
          inline constexpr uintptr_t MousePosition = 0xd4;
-         inline constexpr uintptr_t SensitivityPointer = 0x0;
+         inline constexpr uintptr_t SensitivityPointer = 0x7f90688;
     }
 
     namespace ParticleEmitter {
@@ -383,7 +397,7 @@ namespace Offsets {
          inline constexpr uintptr_t NameDisplayDistance = 0x3a0;
          inline constexpr uintptr_t Team = 0x2d8;
          inline constexpr uintptr_t TeamColor = 0x3ac;
-         inline constexpr uintptr_t UserId = 0x300;
+         inline constexpr uintptr_t UserId = 0xd0;
     }
 
     namespace PlayerConfigurer {
@@ -439,8 +453,8 @@ namespace Offsets {
     }
 
     namespace RunService {
-         inline constexpr uintptr_t HeartbeatFPS = 0xd2;
-         inline constexpr uintptr_t HeartbeatTask = 0x1a0;
+         inline constexpr uintptr_t HeartbeatFPS = 0xf4;
+         inline constexpr uintptr_t HeartbeatTask = 0x24b8;
     }
 
     namespace Script {
@@ -484,7 +498,7 @@ namespace Offsets {
     }
 
     namespace SpawnLocation {
-         inline constexpr uintptr_t AllowTeamChangeOnTouch = 0x3d;
+         inline constexpr uintptr_t AllowTeamChangeOnTouch = 0x188;
          inline constexpr uintptr_t Enabled = 0x189;
          inline constexpr uintptr_t ForcefieldDuration = 0x180;
          inline constexpr uintptr_t Neutral = 0x18a;
@@ -523,7 +537,7 @@ namespace Offsets {
          inline constexpr uintptr_t JobName = 0x18;
          inline constexpr uintptr_t JobStart = 0xc8;
          inline constexpr uintptr_t MaxFPS = 0xb0;
-         inline constexpr uintptr_t Pointer = 0x815d398;
+         inline constexpr uintptr_t Pointer = 0x836aae8;
     }
 
     namespace Team {
@@ -547,10 +561,10 @@ namespace Offsets {
 
     namespace Tool {
          inline constexpr uintptr_t CanBeDropped = 0x4b8;
-         inline constexpr uintptr_t Enabled = 0x395;
+         inline constexpr uintptr_t Enabled = 0x4b9;
          inline constexpr uintptr_t Grip = 0x4ac;
          inline constexpr uintptr_t ManualActivationOnly = 0x4ba;
-         inline constexpr uintptr_t RequiresHandle = 0x245;
+         inline constexpr uintptr_t RequiresHandle = 0x4bb;
          inline constexpr uintptr_t TextureId = 0x360;
          inline constexpr uintptr_t Tooltip = 0x468;
     }
@@ -574,7 +588,7 @@ namespace Offsets {
     namespace VisualEngine {
          inline constexpr uintptr_t Dimensions = 0xab0;
          inline constexpr uintptr_t FakeDataModel = 0xa90;
-         inline constexpr uintptr_t Pointer = 0x82f16a8;
+         inline constexpr uintptr_t Pointer = 0x84f8808;
          inline constexpr uintptr_t RenderView = 0xbb8;
          inline constexpr uintptr_t ViewMatrix = 0x150;
     }
